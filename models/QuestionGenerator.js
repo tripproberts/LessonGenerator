9f6fb828e25ac40de5517fc468d159918396bb53
Tripp Roberts <tripp@tripp-roberts.com>
2017-11-24 15:37:22 -0500

    Add POS tagging so options are generally the same POS

--- models/QuestionGenerator.js
+++ models/QuestionGenerator.js
@@ -1,6 +1,14 @@
 var natural = require('natural'),
     Question = require('./Question'),
-    TfIdf = natural.TfIdf;
+    TfIdf = natural.TfIdf,
+    path = require("path"),
+    base_folder = path.join(path.dirname(require.resolve("natural")), "brill_pos_tagger"),
+    rulesFilename = base_folder + "/data/English/tr_from_posjs.txt",
+    lexiconFilename = base_folder + "/data/English/lexicon_from_posjs.json",
+    defaultCategory = 'N',
+    lexicon = new natural.Lexicon(lexiconFilename, defaultCategory),
+    rules = new natural.RuleSet(rulesFilename),
+    tagger = new natural.BrillPOSTagger(lexicon, rules);
 
 function QuestionGenerator(query, lessonContent) {
   this.query = query;
@@ -14,25 +22,31 @@ QuestionGenerator.prototype.getMultipleChoiceQuestions = function(limit) {
       words = this._getImportantWords(),
       questions = [];
   for (var i = 0; i < limit; i++) {
-    var word = words[i].term,
+    var word = words[Math.floor(Math.random() * 10)].term,
         sentence = self._getSentenceWithWord(word),
         sentence = sentence.replace(word, '________'),
         options = [],
-        answerIndex = Math.floor(Math.random() * 4);
+        answerIndex = Math.floor(Math.random() * 4),
+        attempts = 0;
 
-    while (options.length < 4) {
+    while (options.length < 4 && attempts < 100) {
       if (options.length == answerIndex) {
         options.push(word);
       } else {
         var option = words[Math.floor(Math.random() * words.length)].term;
-        if (!option.includes(word) && !word.includes(option)) {
+        if (self._isValidOption(word, option) && options.indexOf(option) == -1) {
           options.push(option);
         }
       }
+      attempts = attempts + 1;
     }
     questions.push(new Question(sentence, options, answerIndex));
   }
   return questions;
+};
+
+QuestionGenerator.prototype._isValidOption = function(word, option) {
+  return !option.includes(word) && !word.includes(option) && tagger.tag([option])[0][1] == tagger.tag([word])[0][1];
 };
 
 QuestionGenerator.prototype.getTrueFalseQuestions = function(limit) {
@@ -46,7 +60,7 @@ QuestionGenerator.prototype.getTrueFalseQuestions = function(limit) {
         answerIndex = Math.floor(Math.random() * 2);
     if (answerIndex === 1) {
       var option = word;
-      while (option === word) {
+      while (option === word && !self._isValidOption(word, option)) {
         option = words[Math.floor(Math.random() * words.length)].term;
       }
       sentence = sentence.replace(word, option);
